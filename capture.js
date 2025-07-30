8c8c096f965734ad689bf7a0d00013d8f0f8a348
CJCiber <cjciberalumno@gmail.com>
2025-07-30 12:58:44 +0200

    Changes color for the selection

--- capture.js
+++ capture.js
@@ -38,8 +38,8 @@ function startSelection(e) {
     // Crear div de selección
     selectionDiv = document.createElement('div');
     selectionDiv.style.position = 'absolute';
-    selectionDiv.style.border = '.5px dashed #007cba';
-    selectionDiv.style.backgroundColor = 'rgba(0, 124, 186, 0.1)';
+    selectionDiv.style.border = '.5px dashed #ba0000ff';
+    selectionDiv.style.backgroundColor = 'rgba(186, 28, 0, 0.1)';
     selectionDiv.style.left = startX + 'px';
     selectionDiv.style.top = startY + 'px';
     selectionDiv.style.width = '0px';
